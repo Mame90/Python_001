ebc42b5bc9a65d8318d37090bb0691392b59ba36
“[mbaye98]” <mbayediop2309481@gmail.com>
2024-06-13 18:09:50 +0200

    3 eme Commit

--- ClasseAttribut.py
+++ ClasseAttribut.py
@@ -17,3 +17,26 @@ print('Age de h1 -> {}'.format(h1.Age))
 h2 = Humain('Demba Soke',21)
 print('Prenom de h2 -> {}'.format(h2.Prenom))
 print('Age de h2 -> {}'.format(h2.Age))
+
+""" Nous avons aussi ici un code beaucoup plus avancer
+#coding:utf-8
+
+class Humain:
+ 
+ Humaine_creer =0 #initialisation de l'attribut de classe
+
+ def __init__(self,c_prenom,c_nom,c_age):
+  self.prenom=c_prenom
+  self.nom=c_nom
+  self.age=c_age
+  Humain.Humaine_creer +=1 # incrementation de l'attribut de classe
+
+print("lancement du programme")
+h1=Humain('Mbaye','Diop',26)
+print('les attributs du premiers homme est :{} {} ayant {}ans'.format(h1.prenom,h1.nom,h1.age))
+print("Humaine creer est {}".format(Humain.Humaine_creer)) #affichage de l'attribut de classe
+print("----------------*************-------------")
+h2=Humain('Abdou','Faye',25)
+print('les attributs du Deuxieme homme est :{} {} ayant {}ans'.format(h2.prenom,h2.nom,h2.age))
+print("Humaine creer est {}".format(Humain.Humaine_creer))
+"""
